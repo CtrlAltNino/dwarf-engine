9219cf6cab9dbb1391d763ac7e88bbd2258a1eda
Nino-Julian Müller <51022442+flash-miller@users.noreply.github.com>
2023-04-15 20:39:02 +0200

    Github duplicaate directory fix 2/2

---
+++ src/Utilities/PathUtilities.h
@@ -0,0 +1,24 @@
+#pragma once
+
+namespace Dwarf {
+
+    /// @brief Utility functionalities for paths.
+    class PathUtilities {
+        public:
+            /// @brief Converts the backslashes in a path to forward slashes.
+            /// @param path Path to convert.
+            /// @return Converted path.
+            static char* ConvertBackSlashesToForwardSlashes(std::string path){
+                size_t pos;
+                char *cstr = new char[path.length() + 1];
+                
+                while ((pos = path.find('\\')) != std::string::npos) {
+                    path.replace(pos, 1, "/");
+                }
+                
+                strcpy(cstr, path.c_str());
+
+                return cstr;
+            }
+    };
+}
