663155137a6a220733b1e58e5809ab1467798cb5
Nino-Julian Müller <51022442+flash-miller@users.noreply.github.com>
2024-02-26 21:54:10 +0100

    Dependencies version bump

--- src/Core/Scene/Camera.h
+++ src/Core/Scene/Camera.h
@@ -2,6 +2,7 @@
 
 #include <glm/glm.hpp>
 #include <glm/gtc/quaternion.hpp>
+#define GLM_ENABLE_EXPERIMENTAL
 #include <glm/gtx/quaternion.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtx/transform.hpp> // after <glm/glm.hpp>
