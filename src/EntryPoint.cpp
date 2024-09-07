71ee05be77e4febe366347171432e02d70369896
Nino-Julian Müller <51022442+flash-miller@users.noreply.github.com>
2024-09-07 06:41:17 +0200

    Restored Entry Point logging

--- src/EntryPoint.cpp
+++ src/EntryPoint.cpp
@@ -1,24 +1,20 @@
 #include "pch.h"
 #include "Logging/DwarfLogger.h"
 #include "Application/DwarfEngine.h"
-#include <spdlog/spdlog.h>
-#define BOOST_DI_CFG_CTOR_LIMIT_SIZE 20
-#include <boost/di.hpp>
 
 int
 main()
 {
-  // const auto injector = boost::di::make_injector();
-  // const auto logger = injector.create<Dwarf::DwarfLogger>();
+  const auto logger = Dwarf::DwarfLogger(Dwarf::LogName("EntryPoint"));
 
-  // logger.LogInfo(Dwarf::Log("Starting Dwarf Engine...", "EntryPoint"));
+  logger.LogInfo(Dwarf::Log("Starting Dwarf Engine...", "EntryPoint"));
   Dwarf::DwarfEngine engine = Dwarf::DwarfEngine();
-  // logger.LogInfo(Dwarf::Log("Dwarf Engine instance created.", "EntryPoint"));
+  logger.LogInfo(Dwarf::Log("Dwarf Engine instance created.", "EntryPoint"));
 
-  // logger.LogInfo(Dwarf::Log("Running Dwarf Engine...", "EntryPoint"));
+  logger.LogInfo(Dwarf::Log("Running Dwarf Engine...", "EntryPoint"));
   engine.Run();
-  // logger.LogInfo(Dwarf::Log("Dwarf Engine finished running.", "EntryPoint"));
+  logger.LogInfo(Dwarf::Log("Dwarf Engine finished running.", "EntryPoint"));
 
-  // logger.LogInfo(Dwarf::Log("Exiting Dwarf Engine...", "EntryPoint"));
+  logger.LogInfo(Dwarf::Log("Exiting Application...", "EntryPoint"));
   return 0;
 }
