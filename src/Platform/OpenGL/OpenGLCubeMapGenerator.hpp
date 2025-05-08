b117fe8e9f43b79ee4c5e67dba4e749f1d18bd65
Nino-Julian Müller <nino.mue96@gmail.com>
2025-05-08 13:30:37 +0200

    Small progress + procedural skybox

--- src/Platform/OpenGL/OpenGLCubeMapGenerator.hpp
+++ src/Platform/OpenGL/OpenGLCubeMapGenerator.hpp
@@ -6,6 +6,8 @@
 #include "Core/Rendering/Framebuffer/IFramebufferFactory.hpp"
 #include "Core/Rendering/Mesh/IMeshFactory.hpp"
 #include "Core/Rendering/MeshBuffer/IMeshBufferFactory.hpp"
+#include "Core/Rendering/RendererApi/IRendererApi.hpp"
+#include "Core/Rendering/RendererApi/IRendererApiFactory.hpp"
 #include "Core/Rendering/Shader/IShader.hpp"
 #include "Core/Rendering/Shader/ShaderRegistry/IShaderRegistry.hpp"
 #include "Core/Rendering/Texture/ITexture.hpp"
@@ -19,9 +21,11 @@ namespace Dwarf
   private:
     std::shared_ptr<IDwarfLogger>    mLogger;
     std::shared_ptr<ITextureFactory> mTextureFactory;
-    std::shared_ptr<IShader>         mConvertShader;
-    std::shared_ptr<IMeshBuffer>     mCubeMeshBuffer;
-    std::shared_ptr<IFramebuffer>    mFramebuffer;
+    std::shared_ptr<IRendererApi>    mRendererApi;
+
+    std::shared_ptr<IShader>      mConvertShader;
+    std::shared_ptr<IMeshBuffer>  mCubeMeshBuffer;
+    std::shared_ptr<IFramebuffer> mFramebuffer;
 
   public:
     OpenGLCubeMapGenerator(
@@ -29,10 +33,11 @@ namespace Dwarf
       std::shared_ptr<ITextureFactory>        textureFactory,
       const std::shared_ptr<IShaderRegistry>& shaderRegistry,
       const std::shared_ptr<IShaderSourceCollectionFactory>&
-                                                 shaderSourceCollectionFactory,
-      const std::shared_ptr<IMeshFactory>&       meshFactory,
-      const std::shared_ptr<IMeshBufferFactory>& meshBufferFactory,
-      const std::shared_ptr<IFramebufferFactory> framebufferFactory);
+                                                  shaderSourceCollectionFactory,
+      const std::shared_ptr<IMeshFactory>&        meshFactory,
+      const std::shared_ptr<IMeshBufferFactory>&  meshBufferFactory,
+      const std::shared_ptr<IFramebufferFactory>  framebufferFactory,
+      const std::shared_ptr<IRendererApiFactory>& rendererApiFactory);
     ~OpenGLCubeMapGenerator() override = default;
 
     [[nodiscard]] auto
